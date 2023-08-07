1e9c899446d9f5f856d16b81adabe7d3f4f9f597
thexin7 <5349428@qq.com>
2023-08-08 00:28:50 +0800

    fix some compilation issues

--- method/MethodExercise08.java
+++ method/MethodExercise08.java
@@ -1,43 +1,31 @@
-public class MethodExercise08  {
-	public static void main(String[]  args ) {
+public class MethodExercise08 {
+	public static void main(String[]args){
 		Circle a = new Circle();
 		PassObject b = new PassObject();
-		b.printArea(a,  5)
-
-	
-
-
-class Circle  {
-	double radius;
-
-	 
-	public Circle() {
-
-
-	} 
-
-	p
-
-		this.radius = radius; 
-	}    
-
-
-	public double findArea() { 
-		return Math.PI * radius * radius;
+		b.printArea(a,5);
 	}
+}
+class Circle {
+	double radius;
+	public Circle(){
 
-
-	public void set radius(double radius) {
-		this.radius = radius;  
-		
-}    
-
-class PassObject {     
-	pu
-
-		for (int i = 1; i <= times; i++) {
-			c.setradius(i);
-			System.out.println((double) i + "\t" + c.findArea());
+	}
+	public Circle(double radius){
+		this.radius = radius;
+	}
+	public double findArea(){
+		return Math.PI*radius*radius;
+	}
+	public void SetRadius(double radius){
+		this.radius = radius;
+	}
+}
+class PassObject{
+	public void printArea(Circle c, int times){
+		System.out.println("r\tarea");
+		for(int i = 1;i <= times;i++){
+			c.SetRadius(i);
+			System.out.println((double)i+"\t"+c.findArea());
 		}
 
 	}
